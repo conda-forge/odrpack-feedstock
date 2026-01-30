966157432912078e5386c43fc805adb108c535e6
HugoMVale <57530119+HugoMVale@users.noreply.github.com>
2026-01-30 22:01:54 +0100

    add cumtom windows build

---
+++ recipe/bld.bat
@@ -0,0 +1,13 @@
+@echo on
+setlocal
+
+clang-cl.exe --version || exit /b 1
+
+set "CC=clang-cl"
+set "CXX=clang-cl"
+
+%PYTHON% -m pip install . -vv --no-deps --no-build-isolation
+if %ERRORLEVEL% neq 0 (
+  type %CD%\builddir\meson-logs\meson-log.txt
+  exit /b 1
+)
