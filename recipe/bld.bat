57e45f3c93d0da16293c71d38e1c01e0bcaec384
HugoMVale <57530119+HugoMVale@users.noreply.github.com>
2026-01-31 14:05:41 +0100

    revert LIB

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -5,7 +5,7 @@ echo Starting Windows build script...
 
 :: Add the deep-nested Clang/Flang runtime path to the linker search path
 set "LIB=%PREFIX%\Library\lib\clang\21\lib\x86_64-pc-windows-msvc;%LIB%"
-:: set "LIB=%BUILD_PREFIX%\Library\lib\clang\21\lib\x86_64-pc-windows-msvc;%LIB%"
+set "LIB=%BUILD_PREFIX%\Library\lib\clang\21\lib\x86_64-pc-windows-msvc;%LIB%"
 
 clang-cl.exe --version || exit /b 1
 
