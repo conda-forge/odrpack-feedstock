e7dc62d637db0a2ae70c8ec3747bf88c45120480
HugoMVale <57530119+HugoMVale@users.noreply.github.com>
2026-01-31 13:13:27 +0100

    update LIB

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,27 +3,11 @@ setlocal enabledelayedexpansion
 
 echo Starting Windows build script...
 
-:: --- Diagnostic Check Start ---
-echo Checking for Fortran runtime libraries...
+:: Add the deep-nested Clang/Flang runtime path to the linker search path
+set "LIB=%PREFIX%\Library\lib\clang\21\lib\x86_64-pc-windows-msvc;%LIB%"
 
-set "SEARCH_PATH_1=%PREFIX%\Library\lib\flang_rt.runtime.dynamic.lib"
-set "SEARCH_PATH_2=%BUILD_PREFIX%\Library\lib\flang_rt.runtime.dynamic.lib"
-
-if exist "%SEARCH_PATH_1%" (
-    echo [SUCCESS] Found runtime in HOST prefix: %SEARCH_PATH_1%
-) else if exist "%SEARCH_PATH_2%" (
-    echo [WARNING] Found runtime in BUILD prefix: %SEARCH_PATH_2%
-    :: If it's in build but the linker needs it in host, we might need to point to it
-    set "LIB=%BUILD_PREFIX%\Library\lib;%LIB%"
-) else (
-    echo [ERROR] flang_rt.runtime.dynamic.lib not found in standard locations.
-    echo Searching for any flang_rt files in %PREFIX%...
-    dir /s /b "%PREFIX%\*flang_rt*"
-    
-    echo Searching for any flang_rt files in %BUILD_PREFIX%...
-    dir /s /b "%BUILD_PREFIX%\*flang_rt*"
-)
-:: --- Diagnostic Check End ---
+:: Also add the build prefix version just in case
+set "LIB=%BUILD_PREFIX%\Library\lib\clang\21\lib\x86_64-pc-windows-msvc;%LIB%"
 
 clang-cl.exe --version || exit /b 1
 
